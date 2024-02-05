a7925f8c9830a0d724c75bbc5d6fc788d13d4068
Chamath Wijesekera <chamath2.wijesekera@gmail.com>
2024-02-05 10:10:48 -0500

    Clean up proxy.py

--- compute/zeromq/proxy.py
+++ compute/zeromq/proxy.py
@@ -5,7 +5,7 @@ import time
 import eventlet
 
 # Thread for proxying messages between publisher and subscriber
-def proxy(context: zmq.Context):
+def zmq_server(context: zmq.Context):
 
     frontend = context.socket(zmq.XSUB)  # Frontend for publishers
     backend = context.socket(zmq.XPUB)   # Backend for subscribers
@@ -16,39 +16,44 @@ def proxy(context: zmq.Context):
     # Start the proxy
     zmq.proxy(frontend, backend)
 
-# Thread for relaying messages from zeromq to socketio
-def sio(sio: socketio.Server):
+# Thread for relaying messages from zeromq to socketio and back
+def sio_server(sio: socketio.Server, context: zmq.Context):
     app = socketio.WSGIApp(sio)
+    pub: zmq.Socket = context.socket(zmq.PUB)
+    pub.connect("tcp://127.0.0.1:5556")
 
     @sio.on('zmq_message')
     def zmq_message(event, data):
-        sio.emit(data['topic'], data['msg'])
-    
-    eventlet.wsgi.server(eventlet.listen(('', 5557)), app)
+        print(f'[from ZMQ] zmq_{data["topic"]}: {data["msg"]}')
+        sio.emit(f'zmq_{data["topic"]}', data['msg'])
 
-def relay(context: zmq.Context):
+    @sio.on('ui_message')
+    def ui_message(event, data):
+        print(f'[from UI] ui_{data["topic"]}: {data["msg"]}')
+        pub.send_string(f'ui_{data["topic"]} {data["msg"]}')
+
+    eventlet.wsgi.server(eventlet.listen(('', 5557)), app, log_output=False)
+
+def zmq_to_sio(context: zmq.Context):
     sub: zmq.Socket = context.socket(zmq.SUB)
-    pub: zmq.Socket = context.socket(zmq.PUB)
     sub.connect("tcp://127.0.0.1:5555")
-    pub.connect("tcp://127.0.0.1:5556")
-
     sub.setsockopt(zmq.SUBSCRIBE, b"")
 
     sio = socketio.SimpleClient()
     time.sleep(1)
-    sio.connect('http://localhost:5557', wait_timeout=5, transports=['websocket'])
+    sio.connect('http://127.0.0.1:5557', wait_timeout=5, transports=['websocket'])
     with sio:
         while True:
             topic, msg = sub.recv_string().split(' ', 1)
             sio.emit('zmq_message', {'topic': topic, 'msg': msg})
 
 if __name__ == '__main__':
-    zmq_context = zmq.Context()
-    sio_server = socketio.Server(cors_allowed_origins='*')
+    zmq_ctx = zmq.Context()
+    sio_srv = socketio.Server(cors_allowed_origins='*')
 
-    sio_thread = threading.Thread(target=sio, args=(sio_server,))
-    zmq_thread = threading.Thread(target=proxy, args=(zmq_context,))
-    relay_thread = threading.Thread(target=relay, args=(zmq_context,))
+    sio_thread = threading.Thread(target=sio_server, args=(sio_srv, zmq_ctx))
+    zmq_thread = threading.Thread(target=zmq_server, args=(zmq_ctx,))
+    relay_thread = threading.Thread(target=zmq_to_sio, args=(zmq_ctx,))
     zmq_thread.start()
     sio_thread.start()
     relay_thread.start()
