667be25068310041b7bccfb1bdd1d079c5dc2842
Cooper Brunke <cbrunke@uwaterloo.ca>
2024-02-08 17:52:30 -0500

    Finish io script

--- compute/io/main.py
+++ compute/io/main.py
@@ -3,13 +3,13 @@ import gpiozero
 import board
 import busio
 import threading
-import time
-
+from time import sleep
 
 ZMQ_PUB = "tcp://127.0.0.1:5556"
 
 def adc(zmq: zmq.Context, i2c: busio.I2C):
     import adafruit_ads1x15.ads1115 as ADS
+    from adafruit_ads1x15.analog_in import AnalogIn
     adc = ADS.ADS1115(i2c)
 
     chan0 = AnalogIn(adc, ADS.P0)
@@ -19,8 +19,8 @@ def adc(zmq: zmq.Context, i2c: busio.I2C):
     
 
     while True:
-        print(f"chan0 voltage: {chan0.voltage}, chan1 voltage: {chan1.voltage}, chan2 voltage: {chan2.voltage}, chan3 voltage: {chan3.voltage}")
-        time.sleep(0.1)
+        print(f"[ADC] chan0 voltage: {chan0.voltage}, chan1 voltage: {chan1.voltage}, chan2 voltage: {chan2.voltage}, chan3 voltage: {chan3.voltage}")
+        sleep(0.1)
     # pub = setup_zmq_pub(zmq)
 
 def rfid(zmq: zmq.Context, i2c: busio.I2C):
@@ -28,14 +28,16 @@ def rfid(zmq: zmq.Context, i2c: busio.I2C):
     
     pn532 = PN532_I2C(i2c, debug=False)
     pn532.SAM_configuration()
-    pn532.listen_for_passive_target()
+    pn532.listen_for_passive_target(timeout=0.1)
     # Check if a card is available to read
     while True:
         uid = pn532.get_passive_target()
         if uid is not None:
-            print("Found card with UID:", [hex(i) for i in uid])
-
-        pn532.listen_for_passive_target()
+            print("[RFID] Found card with UID:", [hex(i) for i in uid])
+        else:
+            print("[RFID] No RFID")
+        sleep(0.1)
+        pn532.listen_for_passive_target(timeout=0.1)
 
     # pub = setup_zmq_pub(zmq)
 
@@ -45,6 +47,7 @@ def pushbutton(zmq: zmq.Context):
 
     while True:
         print(f'[BUTTON] {pin.value}')
+        sleep(0.1)
 
 
 def imu(zmq: zmq.Context, i2c: busio.I2C):
@@ -57,6 +60,7 @@ def imu(zmq: zmq.Context, i2c: busio.I2C):
     while True:
         accel_x, accel_y, accel_z = bno.acceleration  # pylint:disable=no-member
         print("[IMU] X: %0.6f  Y: %0.6f Z: %0.6f  m/s^2" % (accel_x, accel_y, accel_z))
+        sleep(0.1)
 
 def setup_zmq_pub(context: zmq.Context):
     pub = context.socket(zmq.PUB)
@@ -68,10 +72,10 @@ def main():
     ctx = zmq.Context()
     i2c = busio.I2C(board.SCL, board.SDA)
 
-    adc_thread = threading.Thread(target=adc_thread, args=(ctx, i2c))
-    rfid_thread = threading.Thread(target=rfid_thread, args=(ctx, i2c))
-    pushbutton_thread = threading.Thread(target=pushbutton_thread, args=(ctx,))
-    imu_thread = threading.Thread(target=imu_thread, args=(ctx, i2c))
+    adc_thread = threading.Thread(target=adc, args=(ctx, i2c))
+    rfid_thread = threading.Thread(target=rfid, args=(ctx, i2c))
+    pushbutton_thread = threading.Thread(target=pushbutton, args=(ctx,))
+    imu_thread = threading.Thread(target=imu, args=(ctx, i2c))
 
     adc_thread.start()
     rfid_thread.start()
