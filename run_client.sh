c71fdd7f166c05f8edac45a930f1d4f50b77db54
Chamath Wijesekera <chamath2.wijesekera@gmail.com>
2024-03-14 15:45:34 -0400

    Update run scripts

--- run_client.sh
+++ run_client.sh
@@ -2,11 +2,11 @@ SESSION_NAME="client"
 
 tmux new-session -d -s $SESSION_NAME
 
-tmux send-keys -t $SESSION_NAME "cd power/power_management && sudo -s && . venv/bin/activate && python power_management.py" C-m
+tmux send-keys -t $SESSION_NAME "cd power/power_management && . venv/bin/activate && python power_management.py" C-m
 
 tmux split-window -v
 tmux select-pane -t 1
-tmux send-keys -t $SESSION_NAME "cd compute/io && sudo -s && . venv/bin/activate && python sensors.py" C-m
+tmux send-keys -t $SESSION_NAME "cd compute/io && . venv/bin/activate && python sensors.py" C-m
 
 
 tmux split-window -v
