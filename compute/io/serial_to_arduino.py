40057dcb73ec128542fb1dc1db5d6a0c9ac5690c
Cooper <cbrunke@uwaterloo.ca>
2024-03-23 16:08:34 -0400

    fix type and indent bug

--- compute/io/serial_to_arduino.py
+++ compute/io/serial_to_arduino.py
@@ -7,12 +7,12 @@ import math
 def zmq_to_serial(context: zmq.Context, uart_port: str):
     adc_sub: zmq.Socket = context.socket(zmq.SUB)
     adc_sub.setsockopt(zmq.CONFLATE, 1)
-    adc_sub.connect("tcp://172.20.10.4:5555")
+    adc_sub.connect("tcp://172.20.10.11:5555")
     adc_sub.setsockopt(zmq.SUBSCRIBE, b"adc")
 
     push_sub: zmq.Socket = context.socket(zmq.SUB)
     push_sub.setsockopt(zmq.CONFLATE, 1)
-    push_sub.connect("tcp://172.20.10.4:5555")
+    push_sub.connect("tcp://172.20.10.11:5555")
     push_sub.setsockopt(zmq.SUBSCRIBE, b"push_assist")
 
     uart = serial.Serial(uart_port, 115200)
@@ -35,11 +35,13 @@ def zmq_to_serial(context: zmq.Context, uart_port: str):
             adc_msg: dict = json.loads(adc_msg)
         except ValueError:
             pass
-        
-        if push_msg != 0:
+
+        serial_msg = "0000000"
+        if push_msg != '0':
             #  Calculate PWM for each motor and send to ARDUINO
-            left = adc_msg["channel0"] - adc_msg["channel1"]
-            right = adc_msg["channel2"] - adc_msg["channel3"]
+            print("here")
+            left = float(adc_msg["channel0"]) - float(adc_msg["channel1"])
+            right = float(adc_msg["channel2"]) - float(adc_msg["channel3"])
 
             left = 0 if abs(left) < THRESHOLD else left
             right = 0 if abs(right) < THRESHOLD else right
@@ -57,6 +59,7 @@ def zmq_to_serial(context: zmq.Context, uart_port: str):
 
             serial_msg = directions + 10*right_pwm + left_pwm*10000  # msd lllrrrd lsd (e.g. 1801802)
 
+        print(serial_msg)
         uart.write(f'{serial_msg}\n'.encode())
         # uart.write(json.dumps(msg).encode('utf-8') + b'\n')
         # uart.write(f'{num}\n'.encode())
